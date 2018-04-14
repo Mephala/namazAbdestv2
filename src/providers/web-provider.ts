27b01ddc8bab4919d062366fb079e9a9610cfd4c
Mephala <gokhan.ozgozen@gmail.com>
2018-04-14 07:47:57 +0300

    2.0.1 ready.

--- src/providers/web-provider.ts
+++ src/providers/web-provider.ts
@@ -19,8 +19,8 @@ import {NativeStorage} from "@ionic-native/native-storage";
 export class WebProvider {
 
   appToken: string = "21891fh8291f2812192819h8129f8h34729fh7))_(8128ddh218hf7fh71f21hj1299d218912777898"; //default
-  serverRoot: string = "http://192.168.1.103:8080/namazAppServer";
-  // serverRoot: string = "http://ec2-52-27-157-90.us-west-2.compute.amazonaws.com";
+  // serverRoot: string = "http://192.168.1.105:8080/namazAppServer";
+  serverRoot: string = "http://ec2-52-27-157-90.us-west-2.compute.amazonaws.com";
   version: string = "2.0.1";
   timeout: number = 30000;
   startupData: StartupData;
